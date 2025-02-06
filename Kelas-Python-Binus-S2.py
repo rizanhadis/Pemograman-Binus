6995ecd8a8721ba3b65d42393e0bf7b0d07c54b4
rizanhadist <rizansmage@proton.me>
2025-02-06 19:35:21 +0700

    tugaslab1

--- Kelas-Python-Binus-S2.py
+++
@@ -1,4 +0,0 @@
-def test_me(self):
-        seq = [ 5, 4, 1, 3, 2 ]
-        seq.sort()
-        self.assertEqual(seq, [1, 2, 3, 4, 5])
---
+++ Lembar Jawab.py
@@ -0,0 +1,32 @@
+#Nomor 1
+x = "hello world!"
+g = "Hello"
+h = "World!"
+#Cara 1
+print("Hello World!")
+#Cara 2
+print (x)
+#Cara 3
+print(g,h)
+#Cara 4
+print ("Hello", h)
+#Cara 5
+print (g, "Wolrd!")
+
+#Nomor 2
+x = int(input())
+y = int(input())
+z = x
+x = y 
+y = z 
+print(x, y)
+
+#Nomor 3
+x = input("Masukan kata : ")
+y = input("Masukan kata : ")
+z = input("Masukan kata : ")
+
+#Cara 1
+print(x+"szs", y+"szs", z+"szs" '\n')
+#Cara 2
+print(f"{x}szs {y}szs {z}szs ")
