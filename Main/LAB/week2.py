4e5cffea95856e90be80c2b85b97962044697cff
rizanhadist <rizansmage@proton.me>
2025-02-18 21:35:53 +0700

    update a week2

---
+++ Main/LAB/week2.py
@@ -0,0 +1,27 @@
+
+# nama = input("Masukkan nama: ")
+# nim = input("Masukkan NIM: ")
+# jurusan = input("Masukkan jurusan: ")
+
+# print(f"Perkenalkan nama saya {nama}, NIM saya {nim} dan saya dari jurusan “{jurusan}”")
+
+
+
+# detik = int(input())
+# jam = detik // 3600
+# sisa_detik = detik % 3600
+# menit = sisa_detik // 60
+# detik = sisa_detik % 60
+# print(f"{jam:02d}:{menit:02d}:{detik:02d}")
+
+
+
+celsius = float(input("Masukkan suhu dalam Celsius: "))
+
+reamur = celsius * 0.8
+fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
+
+print(f"Reamur: {reamur}")
+print(f"Fahrenheit: {fahrenheit}")
+print(f"Kelvin: {kelvin}")
